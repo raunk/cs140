289d183607394a8d44fb429d7ea9e3aa0cee7825
Jeremy Keeshin <jkeesh@gmail.com>
2012-02-05 03:21:31 -0800

    starting to add some test lines to syscall

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static void syscall_write(void* esp);
 
 void
 syscall_init (void) 
@@ -16,5 +17,41 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  printf ("esp %p\n", f->esp);
+  int sys_call = *(int*)f->esp;
+  printf("Sys call %d\n", sys_call);
+
+  if(sys_call == SYS_WRITE)
+  {
+    syscall_write(f->esp);
+  }
   thread_exit ();
+}
+
+
+static void
+syscall_write(void* esp)
+{
+  printf("Syscall WRITE\n");
+
+
+  printf("ESP %p\n", esp);
+  printf("+4  %p\n", esp + 4);
+  printf("+8  %p\n", esp + 8);
+  printf("+12 %p\n", esp + 12);
+ 
+  int fd = *(int*)(esp + 4);
+  void* buffer = (void*)(esp + 8);
+  unsigned length = *(unsigned*)(esp + 12);
+
+  printf("FD %d\n", fd);
+  printf("Len %u\n", length); 
+
+
+  if(fd == STDOUT_FILENO)
+  {
+    printf("Write this console using putbuf\n");
+    putbuf(buffer, length); 
+  }
+  
 }

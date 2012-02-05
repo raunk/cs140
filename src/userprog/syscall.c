254c94cfc4556a72677f81c84a3c5d959e5dc8a6
Eric Conner <econner@stanford.edu>
2012-02-05 13:50:39 -0800

    read sys call number

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,28 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
+  // read sys call number from location pointed to by stack pointer
+  int sys_call_number = *((int*)f->esp);
+  switch(sys_call_number) {
+    case SYS_EXIT:
+      printf("Exit system call\n");
+      break;
+    case SYS_WRITE: 
+      printf("Write system call\n");
+      break;
+      /*
+    case SYS_HALT: case SYS_EXEC: case: SYS_CREATE:
+    case SYS_REMOVE: case SYS_OPEN: case SYS_FILESIZE:
+    case SYS_READ: case SYS_SEEK: case SYS_READ:
+    case SYS_WAIT: case SYS_TELL: case SYS_SEEK:
+    case SYS_TELL: case SYS_CLOSE:
+      // TODO
+      break;
+      */
+  }
+  
   printf ("system call!\n");
   thread_exit ();
 }

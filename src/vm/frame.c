83cd5b76e0eaa8e7708e3f9703cef3dc190599bd
Eric Conner <econner@stanford.edu>
2012-02-20 22:16:19 -0800

    ok well it actually compiles.  that was annoying...needed to add file in the threads dir makefile..

---
+++ src/vm/frame.c
@@ -0,0 +1,34 @@
+#include "vm/frame.h"
+#include "threads/palloc.h"
+#include <list.h>
+
+static struct list frame_list;
+
+void
+frame_init(size_t user_page_limit)
+{
+  list_init(&frame_list);
+}
+
+void*
+frame_get_page(enum palloc_flags flags, void *uaddr)
+{
+  /* Ensure we are always getting from the user pool */
+  flags = PAL_USER | flags;
+  
+  /* Attempt to allocate a page, if this comes back null then
+     we need to evict */
+  void *page = palloc_get_page(flags);
+  /* TODO: not sure if this is right way to check that we ran out of pages
+      Maybe we should check if pages within some count?? */
+  if(page != NULL) {
+    struct frame *frm = (struct frame*) malloc(sizeof(struct frame));
+    frm->physical_address = page;
+    frm->user_address = uaddr;
+    frm->owner = thread_current ();
+    
+    list_push_front(&frame_list, &frm->elem);
+  } else {
+    printf("WE RAN OUT OF SPACE. SHIT!\n");
+  }
+}
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,1 +1,20 @@
+#ifndef VM_FRAME_H
+#define VM_FRAME_H
+
+#include <stddef.h>
+#include <list.h>
+
 /* Definitions of page frame */
+struct frame {
+    struct thread *owner;    /* The owner of this frame */
+    void *physical_address;  /* Physical address where frame currently resides. 
+                                This may change if frame needs to be swapped. */
+    void *user_address;      /* User address where thread will access this 
+                                memory */
+    struct list_elem elem;
+};
+
+void frame_init(size_t user_page_limit);
+
+#endif /* vm/frame.h */
+

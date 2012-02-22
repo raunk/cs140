633cabdf358d5e95c06ade352f5e98f87681b759
Charlie Fang <charlief@myth7.Stanford.EDU>
2012-02-21 17:28:41 -0800

    process.c now uses frame.h rather than palloc.h

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -547,14 +547,14 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
       /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
+      uint8_t *kpage = frame_get_page (PAL_USER, upage);
       if (kpage == NULL)
         return false;
 
       /* Load this page. */
       if (safe_file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
-          palloc_free_page (kpage);
+          frame_free_page (kpage);
           return false; 
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
@@ -562,7 +562,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable)) 
         {
-          palloc_free_page (kpage);
+          frame_free_page (kpage);
           return false; 
         }
 
@@ -582,7 +582,6 @@ setup_stack (void **esp)
   uint8_t *kpage;
   bool success = false;
 
-  //kpage = palloc_get_page (PAL_USER | PAL_ZERO);
   kpage = frame_get_page (PAL_USER | PAL_ZERO, ((uint8_t *) PHYS_BASE) - PGSIZE);
   if (kpage != NULL) 
     {
@@ -590,7 +589,7 @@ setup_stack (void **esp)
       if (success)
         *esp = PHYS_BASE;
       else
-        palloc_free_page (kpage);
+        frame_free_page (kpage);
     }
   return success;
 }

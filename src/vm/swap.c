db6ea61ced74cd637ab5aa20a50093124be77ec7
Charlie Fang <charlief@myth2.Stanford.EDU>
2012-02-25 19:59:42 -0800

    changed swap table to use bitmap.h instead of implementing bitmap myself

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,48 +1,23 @@
 #include <debug.h>
+#include <kernel/bitmap.h>
 #include "devices/block.h"
-#include "threads/malloc.h"
 #include "vm/swap.h"
 
-static int is_slot_in_use(swap_slot_t index);
-static void flag_slot(swap_slot_t index, int is_in_use);
 static int get_free_slot_index(void);
 
 static struct block *swap_block;
-static int swap_size;
 
-#define DATA_SIZE 32
-static uint32_t *slot_data;
+/* Bitmap with SWAP_SIZE bits used to keep track of which swap slots
+   are in-use. */
+static struct bitmap *map;
 
 void
 swap_init(void)
 {
   swap_block = block_get_role(BLOCK_SWAP);
-  swap_size = block_size(swap_block);
-  
-  int num_data = 1 + swap_size / DATA_SIZE;
-  slot_data = (uint32_t *) malloc(num_data);
-  if (slot_data == NULL) {
+  map = bitmap_create(block_size(swap_block));
+  if (map == NULL) {
     PANIC("Could not allocate memory for swap table data structure. ");
-  }
-  int i;
-  for (i = 0; i < num_data; i++) {
-    slot_data[i] = 0;
-  }
-}
-
-static int
-is_slot_in_use(swap_slot_t index)
-{
-  return (slot_data[index / DATA_SIZE] & (1 << (index % DATA_SIZE)));
-}
-
-static void
-flag_slot(swap_slot_t index, int is_in_use)
-{
-  if (is_in_use) {
-    slot_data[index / DATA_SIZE] |= (1 << (index % DATA_SIZE));
-  } else {
-    slot_data[index / DATA_SIZE] &= ~(1 << (index % DATA_SIZE));
   }
 }
 
@@ -51,11 +26,12 @@ flag_slot(swap_slot_t index, int is_in_use)
 static int
 get_free_slot_index(void)
 {
-  int index;
-  for (index = 0; index < swap_size; index++) {
-    if (!is_slot_in_use(index)) {
-      flag_slot(index, 1);
-      return index;
+  int swap_size = bitmap_size(map);
+  int idx;
+  for (idx = 0; idx < swap_size; idx++) {
+    if (!bitmap_test(map, idx)) {
+      bitmap_mark(map, idx);
+      return idx;
     }
   }
   
@@ -67,25 +43,25 @@ get_free_slot_index(void)
 int
 swap_write_to_slot(const void *page)
 {
-  int index = get_free_slot_index();
-  if (index >= 0) {
-    block_write(swap_block, index, page); 
+  int idx = get_free_slot_index();
+  if (idx >= 0) {
+    block_write(swap_block, idx, page); 
   }
-  return index;
+  return idx;
 }
 
 /* Copies the page data saved in the swap table at INDEX into BUFFER. */
 void
-swap_read_from_slot(swap_slot_t index, void *buffer)
+swap_read_from_slot(swap_slot_t idx, void *buffer)
 {
-  ASSERT(is_slot_in_use(index));
-  block_read(swap_block, index, buffer);
+  ASSERT(bitmap_test(map, idx));
+  block_read(swap_block, idx, buffer);
 }
 
 /* Flags the slot at index INDEX as free. */
 void
-swap_free_slot(swap_slot_t index)
+swap_free_slot(swap_slot_t idx)
 {
-  ASSERT(is_slot_in_use(index));
-  flag_slot(index, 0);
+  ASSERT(bitmap_test(map, idx));
+  bitmap_reset(map, idx);
 }

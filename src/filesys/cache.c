23a37cb9a120ab2e4108a880b05cec4b4e1121d3
Jeremy Keeshin <jkeesh@gmail.com>
2012-03-10 16:06:21 -0800

    initialize hash for cache

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -1,28 +1,59 @@
 #include "filesys/cache.h"
+#include <debug.h>
 
 
 static struct list cache_list;
 static struct hash cache_hash;
 
-static void cache_evict();
-static void cache_init();
-static int cache_size();
+static void cache_evict(void);
+static void cache_init(void);
+static int cache_size(void);
 static struct cache_elem* cache_lookup(block_sector_t sector);
 static void cache_insert(block_sector_t sector);
 static void cache_reinsert(struct cache_elem* elem);
+static unsigned cache_hash_fn (const struct hash_elem *p_, 
+                                  void *aux UNUSED);
+static bool cache_less_fn (const struct hash_elem *a_, 
+                          const struct hash_elem *b_,
+                          void *aux UNUSED);
+
+
+/* Hash function for cache hash */
+static unsigned 
+cache_hash_fn (const struct hash_elem *p_, void *aux UNUSED)
+{
+  const struct cache_elem* e = 
+      hash_entry(p_, struct cache_elem, hash_elem);
+  return hash_int(e->sector); 
+}
+
+/* Less function for cache hash */
+static bool 
+cache_less_fn (const struct hash_elem *a_, 
+               const struct hash_elem *b_,
+                  void *aux UNUSED)
+{
+  const struct cache_elem* a = 
+      hash_entry(a_, struct cache_elem, hash_elem);
+  const struct cache_elem* b = 
+      hash_entry(b_, struct cache_elem, hash_elem);
+
+  return a->sector < b->sector;
+}
 
 /* Basic setup for the cache */
 static void
 cache_init()
 {
-
+  list_init(&cache_list);
+  hash_init(&cache_hash, cache_hash_fn, cache_less_fn, NULL); 
 }
 
 /* Return number of elements in the cache */
 static int
 cache_size()
 {
-
+  return hash_size(&cache_hash);
 }
 
 /* Lookup sector SECTOR in the cache
